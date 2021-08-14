8a90af0433c6e59161250e8db90afa36071937c0
kelzamoth <kelzamoth@gmail.com>
2021-08-15 01:47:15 +0300

    Changes

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './components/App/App';
 import store from './state/state'
 
-export let renderEntireTree = (state) => {
+export let renderEntireTree = () => {
   ReactDOM.render(
     <React.StrictMode>
       <App state={store.getState()} dispatch={store.dispatch.bind(store)} />
