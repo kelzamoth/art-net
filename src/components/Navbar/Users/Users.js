de6ed6f3a2d9e2f4be4222bcd259675a80f049a1
kelzamoth <kelzamoth@gmail.com>
2021-09-18 22:16:15 +0300

    Add some updates

---
+++ src/components/Navbar/Users/Users.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import s from './Users.module.css'
+
+const Art = (props) => {
+    return (
+        <div>
+            Users
+        </div>
+    )
+}
+
+export default Art;
